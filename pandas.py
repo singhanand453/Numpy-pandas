c4ea4a1972a632273475bb75f3da302f54f5b2da
Anand pratap singh <47928185+singhanand453@users.noreply.github.com>
2019-09-05 23:38:44 +0530

    Update pandas.py

--- pandas.py
+++ pandas.py
@@ -1,14 +1,14 @@
 import numpy as np
-#import time
-#import sys
-#l=range(1000)
-#print(sys.getsizeof(5)*len(l))
-#array=np.arange(1000)
-#print(array.size*array.itemsize)
-#a=np.array([(1,2,3),(4,5,6),(7,8,9)])
-#print(a[0:2,2])
-#for row in a.flat:
-    #print("rows is:",row)
+import time
+import sys
+l=range(1000)
+print(sys.getsizeof(5)*len(l))
+array=np.arange(1000)
+print(array.size*array.itemsize)
+a=np.array([(1,2,3),(4,5,6),(7,8,9)])
+print(a[0:2,2])
+for row in a.flat:
+    print("rows is:",row)
 b=np.arange(10).reshape(5,2)
 c=np.arange(6,12).reshape(3,2)
 
